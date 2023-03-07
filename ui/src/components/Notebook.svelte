cd08005be4224e0a9b9b6c791d4af92ec81a107e
nottherealsanta <ucsantosh@gmail.com>
2023-03-07 08:47:39 +0530

    Cells can arrange themselves

--- ui/src/components/Notebook.svelte
+++ ui/src/components/Notebook.svelte
@@ -1,7 +1,52 @@
 <script lang="ts">
     import Cell from "./Cell.svelte";
 
-    import { notebook } from "../stores/notebook";
+    import { notebook, cells, id_map } from "../stores/notebook";
+
+    function set_locs() {
+        // Iterate through each cell
+        for (let cell_index = 0; cell_index < $cells.length; cell_index++) {
+            // If the cell is not the first cell
+            if (cell_index - 1 >= 0) {
+                // Get the ids of all previous cells
+                let previous_ids = $cells[cell_index].metadata.gm.previous;
+                console.log(previous_ids);
+                // Get the actual cells from the ids
+                let previous_cells = previous_ids.map(
+                    (id) => $cells[$id_map[id]]
+                );
+                // Get the heights of all previous cells
+                let previous_heights = previous_cells.map(
+                    (cell) => cell.metadata.gm.height
+                );
+                // Get the top positions of all previous cells
+                let previous_tops = previous_cells.map(
+                    (cell) => cell.metadata.gm.top
+                );
+                // Get the bottom positions of all previous cells
+                let previous_bottoms = previous_tops.map(
+                    (top, i) => top + previous_heights[i]
+                );
+                console.log(previous_bottoms);
+                // Get the maximum bottom position of all previous cells
+                let max_bottom = Math.max(...previous_bottoms);
+                // Set the top position of the current cell to be 10 pixels
+                // below the maximum bottom position of all previous cells
+                $cells[cell_index].metadata.gm.top = max_bottom + 5;
+            } else {
+                // If the cell is the first cell, set its top position to 100
+                $cells[cell_index].metadata.gm.top = 100;
+            }
+            // Set the left position of the current cell to 100
+            $cells[cell_index].metadata.gm.left = 100;
+            console.log($cells[cell_index].metadata.gm);
+            console.log($notebook["cells"][cell_index].metadata.gm);
+        }
+    }
+
+    // run set_loc 1 second after the notebook is loaded
+    setTimeout(set_locs, 100);
+    // TODO: only if notebook is new to gm
 </script>
 
 <div class="notebook">
@@ -15,5 +60,6 @@
 <style>
     .notebook {
         display: flex;
+        align-items: flex-start;
     }
 </style>
