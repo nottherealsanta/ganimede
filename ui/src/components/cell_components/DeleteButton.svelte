ffba799a6c0d026c3a8450f1bf227a155520aca5
nottherealsanta <ucsantosh@gmail.com>
2023-08-20 11:42:37 +0530

    clean up

--- ui/src/components/cell_components/DeleteButton.svelte
+++ ui/src/components/cell_components/DeleteButton.svelte
@@ -15,11 +15,7 @@
   }}
 >
   <svg viewBox="0 0 21 21" xmlns="http://www.w3.org/2000/svg" fill="#000000"
-    ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
-      id="SVGRepo_tracerCarrier"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    ></g><g id="SVGRepo_iconCarrier">
+    ><g>
       <g
         fill="none"
         fill-rule="evenodd"
--- ui/src/components/cell_components/new_cell_toolbar_components/NewCellToolbar.svelte
+++ ui/src/components/cell_components/new_cell_toolbar_components/NewCellToolbar.svelte
@@ -173,11 +173,7 @@
           viewBox="0 0 512 512"
           width="90%"
           height="90%"
-          ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
-            id="SVGRepo_tracerCarrier"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          ></g><g id="SVGRepo_iconCarrier">
+          ><g>
             <g>
               <path
                 id="p"
