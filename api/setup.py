2499174809f1e924e57f6fe8a5881bfd1cd016c5
nottherealsanta <ucsantosh@gmail.com>
2023-08-22 14:31:53 +0530

    add long_description to setup.py

--- api/setup.py
+++ api/setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.dirname(os.path.abspath(os.path.dirname(__file__)))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\\n" + fh.read()
 
 setup(
     name="ganimede",
