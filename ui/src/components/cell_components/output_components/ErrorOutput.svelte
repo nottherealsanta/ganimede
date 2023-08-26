430e1a56919286db5fa8cf8293deb1fcc5c1c760
nottherealsanta <ucsantosh@gmail.com>
2023-08-26 12:36:13 +0530

    padding changes

--- ui/src/components/cell_components/output_components/ErrorOutput.svelte
+++ ui/src/components/cell_components/output_components/ErrorOutput.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
-    export let output;
+  export let output;
 
-    import { default as AnsiUp } from "ansi_up";
-    const ansi_up = new AnsiUp();
+  import { default as AnsiUp } from "ansi_up";
+  const ansi_up = new AnsiUp();
 </script>
 
-<div class="select-text hover:bg-white/50 dark:hover:bg-black/10">
-    {#if output["traceback"]}
-        {#each output["traceback"] as line}
-            <div class="text-gray-800 dark:text-white whitespace-pre font-mono">
-                {@html ansi_up.ansi_to_html(line)}
-            </div>
-        {/each}
-    {/if}
+<div class="select-text hover:bg-gray-100 dark:hover:bg-zinc-700">
+  {#if output["traceback"]}
+    {#each output["traceback"] as line}
+      <div
+        class="text-gray-800 dark:text-white whitespace-pre font-mono pl-1 pb-0.5"
+      >
+        {@html ansi_up.ansi_to_html(line)}
+      </div>
+    {/each}
+  {/if}
 </div>
